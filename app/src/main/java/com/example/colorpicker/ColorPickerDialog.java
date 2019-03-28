9a2aae662e6fdf558f500cb3722e5b1291b153e6
mathieu <math.per@hotmail.com>
2019-03-28 13:54:02 -0400

    putted the stuff in lambda

--- app/src/main/java/com/example/colorpicker/ColorPickerDialog.java
+++ app/src/main/java/com/example/colorpicker/ColorPickerDialog.java
@@ -2,7 +2,6 @@ package com.example.colorpicker;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
 import android.util.Log;
