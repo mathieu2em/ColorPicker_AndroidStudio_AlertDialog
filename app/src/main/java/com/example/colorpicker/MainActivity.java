2280068e6d21bc9334d77abc0a5843277d624d06
mathieu <math.per@hotmail.com>
2019-03-31 21:33:22 -0400

    corrected a typo in main

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -10,8 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class
-MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     public ColorPickerDialog dialog;
 
